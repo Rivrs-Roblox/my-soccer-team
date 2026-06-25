e1ffbdb8a63cbe5e7263865e90ec18940f16482d
Slobodan <slobodan@rivrs.io>
2026-06-25 11:33:06 +0200

    rebirths and stamina balancing

--- src/shared/Data/Template/Training.lua
+++ src/shared/Data/Template/Training.lua
@@ -21,92 +21,96 @@ return table.freeze({
 			[3] = { RewardPerTick = 158, StaminaCostPerTick = 450 },
 		},
 	},
+
 	["Area02"] = {
 		Shoot = {
 			[1] = { RewardPerTick = 528, StaminaCostPerTick = 15 },
 			[2] = { RewardPerTick = 1_320, StaminaCostPerTick = 750 },
-			[3] = { RewardPerTick = 8_250, StaminaCostPerTick = 37500 },
+			[3] = { RewardPerTick = 8_250, StaminaCostPerTick = 37_500 },
 		},
 		Pass = {
 			[1] = { RewardPerTick = 528, StaminaCostPerTick = 15 },
 			[2] = { RewardPerTick = 1_320, StaminaCostPerTick = 750 },
-			[3] = { RewardPerTick = 8_250, StaminaCostPerTick = 37500 },
+			[3] = { RewardPerTick = 8_250, StaminaCostPerTick = 37_500 },
 		},
 		Dribble = {
 			[1] = { RewardPerTick = 528, StaminaCostPerTick = 15 },
 			[2] = { RewardPerTick = 1_320, StaminaCostPerTick = 750 },
-			[3] = { RewardPerTick = 8_250, StaminaCostPerTick = 37500 },
+			[3] = { RewardPerTick = 8_250, StaminaCostPerTick = 37_500 },
 		},
 		Stamina = {
-			[1] = { RewardPerTick = 528, StaminaCostPerTick = 15 },
-			[2] = { RewardPerTick = 1_320, StaminaCostPerTick = 750 },
-			[3] = { RewardPerTick = 8_250, StaminaCostPerTick = 37500 },
+			[1] = { RewardPerTick = 528, StaminaCostPerTick = 750 },
+			[2] = { RewardPerTick = 2_000, StaminaCostPerTick = 5_250 },
+			[3] = { RewardPerTick = 13_700, StaminaCostPerTick = 67_500 },
 		},
 	},
+
 	["Area03"] = {
 		Shoot = {
 			[1] = { RewardPerTick = 45_000, StaminaCostPerTick = 45 },
-			[2] = { RewardPerTick = 130_000, StaminaCostPerTick = 2250 },
-			[3] = { RewardPerTick = 1_150_000, StaminaCostPerTick = 112500 },
+			[2] = { RewardPerTick = 130_000, StaminaCostPerTick = 2_250 },
+			[3] = { RewardPerTick = 1_150_000, StaminaCostPerTick = 112_500 },
 		},
 		Pass = {
 			[1] = { RewardPerTick = 45_000, StaminaCostPerTick = 45 },
-			[2] = { RewardPerTick = 130_000, StaminaCostPerTick = 2250 },
-			[3] = { RewardPerTick = 1_150_000, StaminaCostPerTick = 112500 },
+			[2] = { RewardPerTick = 130_000, StaminaCostPerTick = 2_250 },
+			[3] = { RewardPerTick = 1_150_000, StaminaCostPerTick = 112_500 },
 		},
 		Dribble = {
 			[1] = { RewardPerTick = 45_000, StaminaCostPerTick = 45 },
-			[2] = { RewardPerTick = 130_000, StaminaCostPerTick = 2250 },
-			[3] = { RewardPerTick = 1_150_000, StaminaCostPerTick = 112500 },
+			[2] = { RewardPerTick = 130_000, StaminaCostPerTick = 2_250 },
+			[3] = { RewardPerTick = 1_150_000, StaminaCostPerTick = 112_500 },
 		},
 		Stamina = {
-			[1] = { RewardPerTick = 45_000, StaminaCostPerTick = 45 },
-			[2] = { RewardPerTick = 130_000, StaminaCostPerTick = 2250 },
-			[3] = { RewardPerTick = 1_150_000, StaminaCostPerTick = 112500 },
+			[1] = { RewardPerTick = 45_000, StaminaCostPerTick = 45_000 },
+			[2] = { RewardPerTick = 170_000, StaminaCostPerTick = 315_000 },
+			[3] = { RewardPerTick = 1_170_000, StaminaCostPerTick = 4_050_000 },
 		},
 	},
+
 	["Area04"] = {
 		Shoot = {
 			[1] = { RewardPerTick = 4_000_000, StaminaCostPerTick = 135 },
-			[2] = { RewardPerTick = 8_000_000, StaminaCostPerTick = 6750 },
-			[3] = { RewardPerTick = 36_000_000, StaminaCostPerTick = 337500 },
+			[2] = { RewardPerTick = 8_000_000, StaminaCostPerTick = 6_750 },
+			[3] = { RewardPerTick = 36_000_000, StaminaCostPerTick = 337_500 },
 		},
 		Pass = {
 			[1] = { RewardPerTick = 4_000_000, StaminaCostPerTick = 135 },
-			[2] = { RewardPerTick = 8_000_000, StaminaCostPerTick = 6750 },
-			[3] = { RewardPerTick = 36_000_000, StaminaCostPerTick = 337500 },
+			[2] = { RewardPerTick = 8_000_000, StaminaCostPerTick = 6_750 },
+			[3] = { RewardPerTick = 36_000_000, StaminaCostPerTick = 337_500 },
 		},
 		Dribble = {
 			[1] = { RewardPerTick = 4_000_000, StaminaCostPerTick = 135 },
-			[2] = { RewardPerTick = 8_000_000, StaminaCostPerTick = 6750 },
-			[3] = { RewardPerTick = 36_000_000, StaminaCostPerTick = 337500 },
+			[2] = { RewardPerTick = 8_000_000, StaminaCostPerTick = 6_750 },
+			[3] = { RewardPerTick = 36_000_000, StaminaCostPerTick = 337_500 },
 		},
 		Stamina = {
-			[1] = { RewardPerTick = 4_000_000, StaminaCostPerTick = 135 },
-			[2] = { RewardPerTick = 8_000_000, StaminaCostPerTick = 6750 },
-			[3] = { RewardPerTick = 36_000_000, StaminaCostPerTick = 337500 },
+			[1] = { RewardPerTick = 4_000_000, StaminaCostPerTick = 2_700_000 },
+			[2] = { RewardPerTick = 15_000_000, StaminaCostPerTick = 18_900_000 },
+			[3] = { RewardPerTick = 103_000_000, StaminaCostPerTick = 243_000_000 },
 		},
 	},
+
 	["Area05"] = {
 		Shoot = {
 			[1] = { RewardPerTick = 195_000_000, StaminaCostPerTick = 405 },
-			[2] = { RewardPerTick = 486_000_000, StaminaCostPerTick = 20250 },
-			[3] = { RewardPerTick = 2_916_000_000, StaminaCostPerTick = 1012500 },
+			[2] = { RewardPerTick = 486_000_000, StaminaCostPerTick = 20_250 },
+			[3] = { RewardPerTick = 2_916_000_000, StaminaCostPerTick = 1_012_500 },
 		},
 		Pass = {
 			[1] = { RewardPerTick = 195_000_000, StaminaCostPerTick = 405 },
-			[2] = { RewardPerTick = 486_000_000, StaminaCostPerTick = 20250 },
-			[3] = { RewardPerTick = 2_916_000_000, StaminaCostPerTick = 1012500 },
+			[2] = { RewardPerTick = 486_000_000, StaminaCostPerTick = 20_250 },
+			[3] = { RewardPerTick = 2_916_000_000, StaminaCostPerTick = 1_012_500 },
 		},
 		Dribble = {
 			[1] = { RewardPerTick = 195_000_000, StaminaCostPerTick = 405 },
-			[2] = { RewardPerTick = 486_000_000, StaminaCostPerTick = 20250 },
-			[3] = { RewardPerTick = 2_916_000_000, StaminaCostPerTick = 1012500 },
+			[2] = { RewardPerTick = 486_000_000, StaminaCostPerTick = 20_250 },
+			[3] = { RewardPerTick = 2_916_000_000, StaminaCostPerTick = 1_012_500 },
 		},
 		Stamina = {
-			[1] = { RewardPerTick = 195_000_000, StaminaCostPerTick = 405 },
-			[2] = { RewardPerTick = 486_000_000, StaminaCostPerTick = 20250 },
-			[3] = { RewardPerTick = 2_916_000_000, StaminaCostPerTick = 1012500 },
+			[1] = { RewardPerTick = 195_000_000, StaminaCostPerTick = 162_000_000 },
+			[2] = { RewardPerTick = 731_000_000, StaminaCostPerTick = 1_134_000_000 },
+			[3] = { RewardPerTick = 5_030_000_000, StaminaCostPerTick = 14_580_000_000 },
 		},
 	},
 })
